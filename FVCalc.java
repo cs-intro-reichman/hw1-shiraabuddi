db0f6206b0356c7c0ecf039db05711ea08ac919a
Omer Abuddi <shira.abuddi@post.runi.ac.il>
2024-11-08 13:02:00 +0200

    test

--- FVCalc.java
+++ FVCalc.java
@@ -3,7 +3,7 @@ public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
 		int currentValue= Math.abs(Integer.parseInt(args[0]));
-		int rate= Integer.parseInt(args[1]);
+		double rate= Double.parseDouble(args[1]);
 		int years= Math.abs(Integer.parseInt(args[2]));
 		double rate1 =(rate/100.0)+1;
 		double futureval = currentValue * Math.pow(rate1, years);
